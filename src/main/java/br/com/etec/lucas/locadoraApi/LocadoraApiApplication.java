1d881b0c4a1d2ea4fb140ff339775f023e5ac3d7
LucasVieiraS <lucasvieirasilva.lpta@gmail.com>
2023-02-15 08:06:28 -0300

    Initial commit

---
+++ src/main/java/br/com/etec/lucas/locadoraApi/LocadoraApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.etec.lucas.locadoraApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoraApiApplication.class, args);
+	}
+
+}
