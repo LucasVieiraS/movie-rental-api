1674baec7ad3c6bbad4115f3b0c3d7dd4bb8f4b6
LucasVieiraS <lucasvieirasilva.lpta@gmail.com>
2023-05-03 08:56:22 -0300

    new: filter fields

--- src/main/java/br/com/etec/lucas/locadoraApi/repository/filter/FilmesFilter.java
+++ src/main/java/br/com/etec/lucas/locadoraApi/repository/filter/FilmesFilter.java
@@ -1,7 +1,10 @@
 package br.com.etec.lucas.locadoraApi.repository.filter;
 
 public class FilmesFilter {
+
   private String nomefilme;
+  private String nomeator;
+  private String nomegenero;
 
   public String getNomefilme() {
     return nomefilme;
@@ -9,5 +12,21 @@ public class FilmesFilter {
 
   public void setNomefilme(String nomefilme) {
     this.nomefilme = nomefilme;
+  }
+
+  public String getNomeator() {
+    return nomeator;
+  }
+
+  public void setNomeator(String nomeator) {
+    this.nomeator = nomeator;
+  }
+
+  public String getNomegenero() {
+    return nomegenero;
+  }
+
+  public void setNomegenero(String nomegenero) {
+    this.nomegenero = nomegenero;
   }
 }
